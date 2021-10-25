39b32451c8fe263fcbbecd94466a8720b16c7d60
kmolan <anmolkathail@gmail.com>
2021-10-25 00:24:19 -0400

    included pine engine changes in gitignore

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
     kind "StaticLib"
     language "C"
     
-	targetdir ("../../bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("../../obj/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("/obj/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
