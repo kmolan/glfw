5ab9f34a9019f09c2327a874ac15b6b0c5271cd0
kmolan <anmolkathail@gmail.com>
2021-10-25 03:31:04 -0400

    Added the correct dependencies for win32

--- premake5.lua
+++ premake5.lua
@@ -9,13 +9,15 @@ project "GLFW"
 	{
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
+        "src/**.h",
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/platform.c",
+        "src/null_**.c"
     }
     
 	filter "system:windows"
@@ -24,12 +26,7 @@ project "GLFW"
         
         files
         {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
+            "src/win32**.c",
             "src/wgl_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c"
